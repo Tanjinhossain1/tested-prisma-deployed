946937f1a4215b32280b697f26058777e7cfe308
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2023-09-02 02:52:37 +0600

    fixed

---
+++ dist/app.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const http_status_1 = __importDefault(require("http-status"));
+// import globalErrorHandler from './app/middlewares/globalErrorHandler';
+// import routes from './app/routes';
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use((0, cookie_parser_1.default)());
+//parser
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+// app.use('/api/v1', routes);
+app.get("/", (_req, res) => {
+    res.send("api is success");
+});
+//global error handler
+// app.use(globalErrorHandler);
+//handle not found
+app.use((req, res, next) => {
+    res.status(http_status_1.default.NOT_FOUND).json({
+        success: false,
+        message: 'Not Found',
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: 'API Not Found',
+            },
+        ],
+    });
+    next();
+});
+exports.default = app;
